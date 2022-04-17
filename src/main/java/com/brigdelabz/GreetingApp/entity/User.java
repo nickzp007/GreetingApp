39cd9d3725460807be2d09dec72304dde2c241a6
unknown <nikhil98patil@gmail.com>
2022-04-17 17:26:03 +0530

    Added Greeting app

---
+++ src/main/java/com/brigdelabz/GreetingApp/entity/User.java
@@ -0,0 +1,15 @@
+package com.brigdelabz.GreetingApp.entity;
+
+public class User {
+
+    private String firstName;
+    private String lastname;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/brigdelabz/GreetingApp/service/GreetingService.java
@@ -0,0 +1,14 @@
+package com.brigdelabz.GreetingApp.service;
+
+import com.brigdelabz.GreetingApp.entity.Greeting;
+import com.brigdelabz.GreetingApp.entity.User;
+
+
+public interface GreetingService {
+
+    Greeting addGreeting(User user);
+    // Greeting getGreeting(long  id);
+    Greeting editGreeting(Long id, String firstName, String lastName);
+    String deleteGreeting(Long id);
+
+}
